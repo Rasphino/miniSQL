9775fd0b76366a0df188c338dc0e023673c46a25
Honghao Li <hh.li99@outlook.com>
2019-06-06 00:38:50 +0800

    [BufferManager]: implement save function & bug fix

--- src/BufferManager.h
+++ src/BufferManager.h
@@ -16,8 +16,14 @@ namespace BM {
         ~BufferManager();
 
         void* read(std::string dbName, uint32_t offset);
+        void save(size_t idx);
+        void setModified(size_t idx);
+        int append(std::string dbName, const Record& row);
 
     private:
+        int getFreeBuffer();
+        void copyToBufferHelper(const Record& row, const CM::table& t, char* p) const;
+
         buffer* buf;
         CM::table* tables[BUF_NUM];
         CM::CatalogManager cm;
